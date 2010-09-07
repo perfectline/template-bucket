e1114b9ea44f99ec77657f2a07b89129acfcae5a
Tanel Suurhans <tanel.suurhans@perfectline.ee>
2010-09-07 16:54:12 +0300

    - some fixups

--- full.rb
+++ full.rb
@@ -1,19 +1,14 @@
-apply 'cleanup'
-apply 'git'
+apply 'cleanup',  :verbose => true
+apply 'git',      :verbose => true
 
 if yes?("Use RSpec?")
-  apply 'rspec.rb'
+  apply 'rspec.rb', :verbose => true
 end
 
 if yes?("Use BDD?")
-  apply 'cucumber.rb'
+  apply 'cucumber.rb', :verbose => true
 end
 
 if yes?("Add jQuery?")
-  apply 'jquery.rb'
+  apply 'jquery.rb', :verbose => true
 end
-
-#if yes?("Add jQuery UI?")
-  #apply 'jqueryui.rb'
-#end
-
