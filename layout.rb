71417987fa3e9fb95e20fc9f99f3b13ab647ebd1
J. Edward Dewyea <jed@commuterdesign.com>
2010-10-19 16:35:39 -0400

    :defaults will include application.js

--- layout.rb
+++ layout.rb
@@ -6,16 +6,16 @@ inside "app/views/layouts" do
   if @use_haml
     get "#{File.dirname(__FILE__)}/resources/layout.html.haml", "application.html.haml"
 
-    gsub_file "application.html.haml", /\/ insert javascript here\n/, "= include_javascripts :application\n"                if @use_jammit.present?
-    gsub_file "application.html.haml", /\/ insert javascript here\n/, "= javascript_include_tag :defaults, 'application'\n" if @use_jammit.nil?
+    gsub_file "application.html.haml", /\/ insert javascript here\n/, "= include_javascripts :application\n" if @use_jammit.present?
+    gsub_file "application.html.haml", /\/ insert javascript here\n/, "= javascript_include_tag :defaults\n" if @use_jammit.nil?
 
     gsub_file "application.html.haml", /\/ insert stylesheet here\n/, "= include_stylesheets :application\n"  if @use_jammit.present?
     gsub_file "application.html.haml", /\/ insert stylesheet here\n/, "= stylesheet_link_tag 'reset'\n"       if @use_jammit.nil?
   else
     get "#{File.dirname(__FILE__)}/resources/layout.html.erb", "application.html.erb"
 
-    gsub_file "application.html.erb", /<!-- insert javascript here -->\n/, "<%= include_javascripts :application %>\n"                  if @use_jammit.present?
-    gsub_file "application.html.erb", /<!-- insert javascript here -->\n/, "<%= javascript_include_tag :defaults, 'application' %>\n"   if @use_jammit.nil?
+    gsub_file "application.html.erb", /<!-- insert javascript here -->\n/, "<%= include_javascripts :application %>\n" if @use_jammit.present?
+    gsub_file "application.html.erb", /<!-- insert javascript here -->\n/, "<%= javascript_include_tag :defaults %>\n" if @use_jammit.nil?
 
     gsub_file "application.html.erb", /<!-- insert stylesheet here -->\n/, "<%= include_stylesheets :application %>\n" if @use_jammit.present?
     gsub_file "application.html.erb", /<!-- insert stylesheet here -->\n/, "<%= stylesheet_include_tag 'reset' %>\n"   if @use_jammit.nil?
