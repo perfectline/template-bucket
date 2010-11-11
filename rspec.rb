1bbf59ca2311935d17599a3f0d89f5f452e3d2e2
J. Edward Dewyea <jed@commuterdesign.com>
2010-11-10 21:10:15 -0500

    install factory_girl_rails since we're using Rails 3

--- rspec.rb
+++ rspec.rb
@@ -46,7 +46,7 @@ if yes?("Install rspec and rspec-rails?", :yellow)
 end
 
 if yes?("Install factory_girl?", :yellow)
-  gem 'factory_girl', '>= 1.3.2', :group => :test
+  gem 'factory_girl_rails', :group => :test
 
   inject_into_file "config/application.rb", :after => "config.generators do |generator|\n" do
     (" " * 6) + "generator.fixture_replacement :factory_girl, :dir => '#{@use_rspec ? "spec/factories" : "test/factories"}'\n"
